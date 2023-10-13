788ec093fdf3b26214390c3313c758adb243b068
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-13 11:36:41 +0200

    working with faker

--- src/main/java/emmek/Application.java
+++ src/main/java/emmek/Application.java
@@ -12,13 +12,11 @@ public class Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Library library = new Library();
         Faker faker = new Faker();
         Random rnd = new Random();
-        System.out.println(faker.date().birthday("yyyy"));
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             Periodicity[] periodicity = Periodicity.values();
             if (rnd.nextBoolean()) {
                 library.add(new Book(faker.code().isbn10(),
@@ -30,7 +28,7 @@ public class Application {
                 ));
             } else {
                 library.add(new Magazine(faker.code().isbn10(),
-                        faker.book().title(),
+                        faker.yoda().quote(),
                         faker.date().birthday("yyyy"),
                         faker.number().numberBetween(16, 100),
                         periodicity[rnd.nextInt(periodicity.length)]
